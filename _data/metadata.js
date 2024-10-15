27896e8673c4438a97a0df6cc88f026299878b2e
Mr-Hei7enberg <shebanits123456@gmail.com>
2024-10-15 23:34:25 +0300

    v1.2

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "CPTL",
+	url: "https://www.cptl.com.ua/",
+	language: "ua",
+	description: "",
+	author: {
+		name: "CPTL",
+		email: "office@cptl.com.ua",
+		url: "https://www.cptl.com.ua/about/"
+	}
+}
